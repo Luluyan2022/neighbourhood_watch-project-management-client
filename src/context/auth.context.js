0368781b17f0c0392a41ce5888f7085c04eb6139
Luluyan2022 <emmymagao@gmail.com>
2023-01-27 13:14:29 +0100

    fixed the probleme: token expired, can not access to the loginPage

--- src/context/auth.context.js
+++ src/context/auth.context.js
@@ -3,27 +3,32 @@ import { createContext, useEffect, useState } from "react";
 
 const AuthContext = createContext();
 
-function AuthProviderWrapper(props){
+function AuthProviderWrapper(props) {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [isLoading, setIsloading] = useState(true);
     const [user, setUser] = useState(null);
 
-    const storeToken = (token => {        
+    const storeToken = (token => {
         localStorage.setItem("authToken", token);
     })
 
     const authenticateUser = () => {
         const storedToken = localStorage.getItem("authToken");
-        if(storedToken){
+        if (storedToken) {
             // if the token exists
-            axios.get(`${process.env.REACT_APP_API_URL}/auth/verify`, {headers: {Authorization: `Bearer ${storedToken}`}})
-                 .then((res) => {
+            axios.get(`${process.env.REACT_APP_API_URL}/auth/verify`, { headers: { Authorization: `Bearer ${storedToken}` } })
+                .then((res) => {
                     const user = res.data;
                     setIsLoggedIn(true);
                     setIsloading(false);
                     setUser(user)
-                 })
-        }else{
+                })
+                .catch((error) => {
+                    setIsLoggedIn(false)
+                    setIsloading(false)
+                    setUser(user)
+                })
+        } else {
             // If the token is not available (or is removed)
             setIsLoggedIn(false);
             setIsloading(false);
@@ -44,11 +49,11 @@ function AuthProviderWrapper(props){
     }, []);
 
     return (
-        <AuthContext.Provider value={{isLoggedIn, isLoading, user, storeToken, authenticateUser, logOutUser}}>
+        <AuthContext.Provider value={{ isLoggedIn, isLoading, user, storeToken, authenticateUser, logOutUser }}>
             {props.children}
         </AuthContext.Provider>
     )
 
 }
 
-export {AuthProviderWrapper, AuthContext}
+export { AuthProviderWrapper, AuthContext }
