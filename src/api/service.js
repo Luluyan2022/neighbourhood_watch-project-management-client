9b96e01df22b5ec7fb46392075c370abfa37ce24
Luluyan2022 <emmymagao@gmail.com>
2023-01-31 14:34:58 +0100

    changed the url in service.js

--- src/api/service.js
+++ src/api/service.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const api = axios.create({
   
-  baseURL: "http://localhost:5005/api"
+  baseURL: process.env.REACT_APP_API_URL + "/api"
   // withCredentials: true // => you might need this option if using cookies and sessions
 });
 
