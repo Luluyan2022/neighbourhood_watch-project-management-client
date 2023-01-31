f0e074431805e7bffd8043983e110ed82d4b2731
Luluyan2022 <emmymagao@gmail.com>
2023-01-31 16:46:30 +0100

    improved the user experience when user click like

--- src/components/DiscoveryDetailsPart.js
+++ src/components/DiscoveryDetailsPart.js
@@ -5,14 +5,13 @@ import heartIcon from "../images/heart.png"
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/auth.context";
 export default function DiscoveryDetailsPart(props) {
-    const navigate = useNavigate(); 
+    const navigate = useNavigate();
     const [likerArr, setLikerArr] = useState([]);
-    const { discoveryId } = useParams();  
-    const {user} = useContext(AuthContext)
+    const { discoveryId } = useParams();
+    const { user } = useContext(AuthContext)
 
-    //get prevCounterArr and display the number of likers
-
-    const getLikerArr= () => {
+    //get prevCounterArr and display the number of likers 
+    const getLikerArr = () => {
         const storedToken = localStorage.getItem('authToken');
         axios.get(`${process.env.REACT_APP_API_URL}/api/discoveries/${discoveryId}`, { headers: { Authorization: `Bearer ${storedToken}` } })
             .then(res => {
@@ -20,31 +19,31 @@ export default function DiscoveryDetailsPart(props) {
                 console.log(prevLikerArr)
                 setLikerArr(prevLikerArr)
             })
-            .catch((error) => console.log("error in getting likerArr",error));
+            .catch((error) => console.log("error in getting likerArr", error));
     }
     // eslint-disable-next-line
-    useEffect(() => {getLikerArr()}, [discoveryId])    
+    useEffect(() => { getLikerArr() }, [discoveryId])
 
-    const increaseCounter = () => {
-       
+    // user click the like button
+    const increaseCounter = (e) => {
+        e.preventDefault()
         if (likerArr.includes(user.name) === false) {
-            setLikerArr(prevLikerArr => {
-                const newArr = prevLikerArr.push(user.name);
-                return newArr
-            })
-            const requestBody = { likerArr };
+
+            let newLikeArr = [...likerArr, user.name]
+            const requestBody = { likerArr: newLikeArr };
+
             const storedToken = localStorage.getItem('authToken');
             axios
                 .put(`${process.env.REACT_APP_API_URL}/api/discoveries/edit/${discoveryId}`, requestBody, { headers: { Authorization: `Bearer ${storedToken}` } })
-                .then(() => {
-                    console.log("updated LikerArr");
-                    getLikerArr();
+                .then((res) => {
+                    setLikerArr(res.data.likerArr)
+
                 })
                 .catch((error) => console.log("error in increasing likerNumber", error));
 
-        }else{
+        } else {
             return likerArr
-          }  
+        }
     }
 
     const deleteDiscovery = () => {
@@ -60,7 +59,7 @@ export default function DiscoveryDetailsPart(props) {
         <div>
             <div>
                 {props.discovery &&
-                    <div>                        
+                    <div>
                         {!props.discovery.imageUrl
                             ? <img id="discDetaiImage" src="https://via.placeholder.com/1150" alt="no pictures here" />
                             : <img id="discDetaiImage" src={props.discovery.imageUrl} alt="object" />
@@ -68,22 +67,22 @@ export default function DiscoveryDetailsPart(props) {
                         <div id="discDetaiTxt">
                             <div>
                                 <h1 className="m-3 mb-5">{props.discovery.title}</h1>
-                            </div>                            
+                            </div>
                             <div>
                                 <p>{props.discovery.description}</p>
                             </div>
-                            <form style={{ display:'flex', flexDirection:'row', position:'absolute', top:'10vh', left:'32vw'}}> 
+                            <form style={{ display: 'flex', flexDirection: 'row', position: 'absolute', top: '10vh', left: '32vw' }}>
                                 <button className="border-0 me-3" onClick={increaseCounter} ><img src={heartIcon} alt="like" style={{ width: '2em' }} className="m-3 mb-3" /></button>
-                                <h2 className="mt-2">{likerArr.length!==0 && props.discovery.likerArr?.length}</h2>
+                                <h2 className="mt-2">{likerArr.length !== 0 && likerArr?.length}</h2>
                             </form>
                         </div>
                     </div>
                 }
-            </div>          
+            </div>
 
             <div className="my-2">
                 <Button onClick={deleteDiscovery} className="px-5" >Delete</Button>
-            </div>           
+            </div>
         </div>
     )
 }
